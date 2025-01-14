7861b449b0305ebcf4dca4953181e90b69c8f8d0
zsoltkulcsar <kulcsarzsolt15@gmail.com>
2025-01-14 23:25:27 +0200

    Create file

---
+++ 135-Candy/135-Candy.cs
@@ -0,0 +1,33 @@
+public class Solution {
+       public int Candy(int[] ratings)
+    {
+        int n = ratings.Length;
+        int[] candies = new int[n];
+        Array.Fill(candies, 1); // Each child gets at least one candy
+
+        // Traverse from left to right
+        for (int i = 1; i < n; i++)
+        {
+            if (ratings[i] > ratings[i - 1])
+            {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            if (ratings[i] > ratings[i + 1])
+            {
+                candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int totalCandies = 0;
+        foreach (int candy in candies)
+        {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
+    }
+}
