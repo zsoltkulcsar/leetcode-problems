6ab6ca2d320ead8761d99be91fc1422cf68ea358
zsoltkulcsar <kulcsarzsolt15@gmail.com>
2025-01-27 22:32:59 +0200

    Create file

---
+++ 48-RotateImage/48-RotateImage.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public void Rotate(int[][] matrix) {
+        int n = matrix.Length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            Array.Reverse(matrix[i]);
+        }
+    }
+}
